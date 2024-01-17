d0c8d9b2e9bab18cd7dc9601d6e7c06374b9d74d
Dmitriy Belozerov <dm.belozerov@yandex.ru>
2024-01-17 20:24:31 +0300

    validation form

--- FRONTEND/src/component/Form.js
+++ FRONTEND/src/component/Form.js
@@ -1,15 +1,16 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { ErrorMessage } from "@hookform/error-message";
 
 function Form(props) {
     const [titleProduct, setTitleProduct] = React.useState('');
     const [titlePrice, setTitlePrice] = React.useState('');
     const [lastName, setLastName] = React.useState('');
+    const { register, handleSubmit, formState: { errors } } = useForm();
 
 
 
-
-    function handleSubmitProduct(e) {
-        e.preventDefault();
+    const handleSubmitForm = _ => {
         props.onSubmitProduct(titleProduct, titlePrice, lastName);
         setTitleProduct('');
         setTitlePrice('');
@@ -30,16 +31,25 @@ function Form(props) {
 
     return (
         <section>
-            <form className="form" onSubmit={handleSubmitProduct}>
-                <input placeholder="Введите название продукта" onChange={handleChangeProduct} value={titleProduct || ''} required maxLength='30'></input>
-                <input type="number" placeholder="Введите сумму" onChange={handleChangePrice} value={titlePrice || ''} required></input>
-                <select required onChange={handleChangeLastname} value={lastName}>
+            <form className="form" onSubmit={handleSubmit(handleSubmitForm)}>
+                <input {...register('name', { required: true, maxLength: 40 })} type="text" placeholder="Введите название продукта" onChange={handleChangeProduct} value={titleProduct || ''}></input>
+                {errors.name && <span className="form__error">Поле "Название продукта" обязательно к заполнению</span>}
+                <ErrorMessage 
+                    errors={errors}
+                    name="name"
+                    render={({ message }) => <p>{message}</p>}
+                />
+                <input {...register('price', { required: true, min: 1, max: 10000 })} type="number" placeholder="Введите сумму" onChange={handleChangePrice} value={titlePrice || ''} ></input>
+                {errors.price && <span className="form__error">Поле "Сумма" обязательно к заполнению</span>}
+                <select {...register('lastName', { required: true })} onChange={handleChangeLastname} value={lastName}>
                     <option disabled selected ></option>
                     <option value={'belozerov'}>Белозеровы</option>
                     <option value={'ustinov'}>Устиновы</option>
                     <option value={'trofimov'}>Трофимовы</option>
                 </select>
-                <button className="form__button">Добавить</button>
+                {errors.lastName && <span className="form__error">Поле "Фамилия" обязательно к заполнению</span>}
+
+                <button className="form__button" type="submit">Добавить</button>
             </form>
         </section>
     )
